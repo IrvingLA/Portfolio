081542082cd48742a7d8c44fa4db8ce4b00aacbf
IrvingLA <ibarramorenoirving770@gmail.com>
2026-07-05 22:36:58 -0600

    feat: add interactive dialog to about me skills cards with new sections

--- src/constants/about-me.ts
+++ src/constants/about-me.ts
@@ -1,4 +1,6 @@
 import prota from "../assets/avatars/prota.png";
+import yukari from "../assets/avatars/yukari.png";
+import junpei from "../assets/avatars/junpei.png";
 
 export interface Skills {
   title: string;
@@ -14,10 +16,28 @@ export const section: Skills[] = [
   {
     title: "Sobre mi",
     urlImage: prota.src,
-    altImage: "Imagen del protagonista",
-    lv: 40,
-    hp: 390,
-    sp: 250,
-    color: "primary",
+    altImage: "Retrato de Sobre mí",
+    lv: 55,
+    hp: 550,
+    sp: 350,
+    color: "bg-primary-300/60",
+  },
+  {
+    title: "technical skills",
+    urlImage: yukari.src,
+    altImage: "Retrato de Technical Skills",
+    lv: 42,
+    hp: 400,
+    sp: 280,
+    color: "bg-pink-500/50",
+  },
+  {
+    title: "soft skills",
+    urlImage: junpei.src,
+    altImage: "Retrato de Soft Skills",
+    lv: 48,
+    hp: 450,
+    sp: 320,
+    color: "bg-blue-500/50",
   },
 ];
