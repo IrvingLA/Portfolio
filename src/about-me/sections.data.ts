7c3b8b280331e2fe70d60d9db86d07c79ce09159
IrvingLA <ibarramorenoirving770@gmail.com>
2026-07-08 20:55:17 -0600

    refactor: reorganize project into Scream Architecture domains

--- src/about-me/sections.data.ts
+++ src/about-me/sections.data.ts
@@ -1,14 +1,14 @@
-import protaAvatar from "../assets/avatars/prota.png";
-import yukariAvatar from "../assets/avatars/yukari.png";
-import junpeiAvatar from "../assets/avatars/junpei.png";
+import protaAvatar from "./assets/avatars/prota.png";
+import yukariAvatar from "./assets/avatars/yukari.png";
+import junpeiAvatar from "./assets/avatars/junpei.png";
 
-import protaShadow from "../assets/character/prota-shadow.webp";
-import yukariShadow from "../assets/character/yukari-shadow.webp";
-import junpeiShadow from "../assets/character/junpei-shadow.webp";
+import protaShadow from "./assets/character/prota-shadow.webp";
+import yukariShadow from "./assets/character/yukari-shadow.webp";
+import junpeiShadow from "./assets/character/junpei-shadow.webp";
 
-import AboutMeText from "../components/about-me-text.astro";
-import SoftSkills from "../components/soft-skills.astro";
-import TechnicalSkills from "../components/technical-skills.astro";
+import AboutMeText from "./AboutMeText.astro";
+import SoftSkills from "./SoftSkills.astro";
+import TechnicalSkills from "./TechnicalSkills.astro";
 import type { AstroComponentFactory } from "astro/runtime/server/index.js";
 
 export interface Skills {
